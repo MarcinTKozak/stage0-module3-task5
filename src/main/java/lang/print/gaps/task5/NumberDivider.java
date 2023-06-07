7570c761aab48fdf5e8f3b2711cdddbdc8a1adea
Marcin <marcin.t.kozak@gmail.com>
2023-06-07 21:50:55 +0100

    changing int to double 2

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(int toBeDivided) {
 
-        double score = toBeDivided / 5;
+        double score = toBeDivided / 5.0;
         System.out.println(score);
     }
 }
